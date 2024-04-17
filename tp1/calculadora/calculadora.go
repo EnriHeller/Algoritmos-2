f4b55d0aaadd4570fe02d3ffe2a0ecaea4cb8ec8
EnriHeller <enrique.heller12@gmail.com>
2024-04-17 09:33:48 -0300

    duplicated tp1 deleted

--- tp1/calculadora/calculadora.go
+++
@@ -1,7 +0,0 @@
-package calculadora
-
-type Calculadora interface {
-
-	//A partir de una operación en notación polaca inversa, Operar devuelve resultado y un booleano que indica la existencia de un error o no.
-	Operar(comando string) (int64, bool)
-}
